c96181035b935e3c68fa139ef51cfe9127e2a0fd
Prerna <prerna1998.ps@gmail.com>
2019-10-13 23:32:46 +0530

    Accessing all command line arguments

---
+++ Command_Line_Arguments/secondProgram.java
@@ -0,0 +1,9 @@
+//Java Program to display all command line arguments dynamically.
+class secondProgram {
+	public static void main(String args[]) {
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
+		}
+	}
+
+}
